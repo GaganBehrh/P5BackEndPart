c30421232dffb30ee3f919a9f7c9a53e9696344f
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2023-03-14 06:26:21 +0000

    RecipePost model addition under progress

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Profile
+from .models import Profile,RecipePost
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -10,4 +10,17 @@ class ProfileSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
             'content', 'image'
+        ]
+
+
+class RecipePostSerializer(serializers.ModelSerializer):
+    profile_name = serializers.ReadOnlyField(source='owner.profile.name')
+    profile_content = serializers.ReadOnlyField(source='owner.profile.content')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    class Meta:
+        model = RecipePost
+        fields = [
+            'id', 'owner', 'created_on', 'updated_on', 'title',
+            'matter', 'pic'
         ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Profile
+from .models import Profile,RecipePost 
 from .serializers import ProfileSerializer
 
 
@@ -13,3 +13,13 @@ class ProfileList(APIView):
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
+
+class RecipeList(APIView):
+    """
+    List all profiles
+    No Create view (post method), as profile creation handled by django signals
+    """
+    def get(self, request):
+        recipes = RecipePost.objects.all()
+        recipeserializer = RecipePostSerializer(recipes, many=True)
+        return Response(recipeserializer.data)
