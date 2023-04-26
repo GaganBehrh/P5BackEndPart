ce2dcb1e463e27b5d3b0ae5e12747c8ede88622d
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2023-04-26 04:26:39 +0000

    CLient origin changes to client_orig dev

--- p5django/settings.py
+++ p5django/settings.py
@@ -109,7 +109,7 @@ MIDDLEWARE = [
 ]
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
-         os.environ.get('CLIENT_ORIGIN')
+         os.environ.get('CLIENT_ORIGIN_DEV')
      ]
 if 'CLIENT_ORIGIN_DEV' in os.environ:
     extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
